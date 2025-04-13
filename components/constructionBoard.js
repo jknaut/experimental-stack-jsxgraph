7a538c4a511103416e6c9edd2f7f8a5c855d0ae6
jknaut <109731454+jknaut@users.noreply.github.com>
2025-04-13 23:04:11 +0200

    Change anchorX

--- components/constructionBoard.js
+++ components/constructionBoard.js
@@ -578,7 +578,7 @@ var createConstructionBoard = (userOptions = {}) => {
             if (i % 2 == 1) {
                 vecs[j] = board.create("arrow", [ps[i - 1], ps[i]], { ...vecStyle, withLabel: false, color: col1 });
                 mps[i] = board.create("midpoint", [ps[i-1], ps[i]], {visible: false, highlight: false, size: 0});
-                mpLabels[i] = board.create("text", [() => mps[i].X(), () => mps[i].Y(), vecLabels[j]], {anchorX: 'middle', anchorY: 'middle', color: "white", cssStyle: `background-color: ${collight1}; padding: 2px 4px; border-radius: 4px;`});
+                mpLabels[i] = board.create("text", [() => mps[i].X(), () => mps[i].Y(), vecLabels[j]], {anchorX: 'left', anchorY: 'middle', color: "white", cssStyle: `background-color: ${collight1}; padding: 2px 4px; border-radius: 4px;`});
                 j = j + 1;
             }
         }
